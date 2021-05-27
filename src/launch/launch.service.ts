970c612e5d5c3f48b53bec3a55ae44487f6924c1
Ron Almog <ron8132@gmail.com>
2021-05-26 22:36:58 -0400

    service, resolver

--- src/launch/launch.service.ts
+++ src/launch/launch.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LaunchService {}
+export class LaunchService {
+  private apiUrl = 'https://api.spacexdata.com/v3';
+
+  constructor(private http: HttpService) {}
+
+  getAllLaunches() {
+    return this.http.get(`${this.apiUrl}/launches`);
+  }
+
+  getLaunchById(id: number) {
+    return this.http.get(`${this.apiUrl}/launches/${id}`);
+  }
+}
